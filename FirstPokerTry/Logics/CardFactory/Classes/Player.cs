e63ad7a708e71e44e40b17ad5e6e14016199312d
carolinevannebo <58633333+carolinevannebo@users.noreply.github.com>
2022-11-07 17:54:21 +0100

    Fixing version control problems

---
+++ FirstPokerTry/Logics/CardFactory/Classes/Player.cs
@@ -0,0 +1,37 @@
+﻿#nullable disable
+
+using System;
+using FirstPokerTry.Logics.CardFactory.Interfaces;
+using FirstPokerTry.Logics.Objects;
+using System.ComponentModel.DataAnnotations;
+
+namespace FirstPokerTry.Logics.CardFactory.Classes
+{
+    public class Player : IPlayer
+    {
+        #region Properties
+
+        [Key]
+        public int Id { get; }
+        public string Name { get; }
+        private PlayerObject PlayerObject { get; set; }
+
+        #endregion
+
+        public Player(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        #region Methods
+
+        public void SetUser(PlayerObject playerObject)
+        {
+            PlayerObject = playerObject;
+        }
+
+        #endregion
+    }
+}
+
---
+++ FirstPokerTry/Logics/Objects/PlayerObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace FirstPokerTry.Logics.Objects
+{
+    public class PlayerObject
+    {
+        #region Properties
+        private readonly int _id;
+        private string _name;
+        #endregion
+
+        public PlayerObject(int id, string name)
+        {
+            _id = id;
+            _name = name;
+        }
+    }
+}
+
