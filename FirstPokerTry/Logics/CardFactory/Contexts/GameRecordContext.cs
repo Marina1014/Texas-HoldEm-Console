aad296445c2ec3451c1418008b9e8e8303353359
carolinevannebo <58633333+carolinevannebo@users.noreply.github.com>
2022-11-07 19:43:10 +0100

    Changed context and properties for db

---
+++ FirstPokerTry/Logics/CardFactory/Contexts/GameRecordContext.cs
@@ -0,0 +1,22 @@
+﻿#nullable disable
+
+using System;
+using Microsoft.EntityFrameworkCore;
+using FirstPokerTry.Logics.CardFactory.Classes;
+
+namespace FirstPokerTry.Logics.CardFactory.Contexts
+{
+    public class GameRecordContext : DbContext
+    {
+        //public GameRecordContext(DbContextOptions<GameRecordContext> options) : base(options) { }
+
+        public DbSet<Player> Player => Set<Player>();
+        public DbSet<Game> Game => Set<Game>();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = ../../../FirstPokerTry/Data/Database/GameRecord.db");
+        }
+    }
+}
+
