6fa6ee83e15036f71e3817725c816a8c79ddc76d
taraav <tara.vinje@gmail.com>
2022-11-21 17:13:34 +0100

    Added test for JsonCardDeckFileReader

---
+++ FirstPokerTryTest/JsonReaderTest.cs
@@ -0,0 +1,32 @@
+﻿using FirstPokerTry.Logics.Gameplay;
+using System;
+using FirstPokerTry.Logics.CardFactory.Classes;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using FirstPokerTry.Logics.Objects;
+using FirstPokerTry.Data;
+using FirstPokerTry.Data.Json;
+
+namespace FirstPokerTryTest
+{
+	public class JsonReaderTest
+	{
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void ShouldReturnCardDeck()
+        {
+            var cardDeck = JsonCardDeckFileReader.GetJsonCardDeck();
+                      
+            int result = 52;
+
+            Assert.That(result, Is.EqualTo(cardDeck.Count));
+
+        }
+    }
+}
+
